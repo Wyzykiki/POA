18d0badd22a3f990724b95e6ea56aded46a92816
Kyllian <47327666+Wyzykiki@users.noreply.github.com>
2020-04-03 17:45:48 +0200

    Initialisation laby complete Gardes, affiches, boites

--- Labyrinthe.cc
+++ Labyrinthe.cc
@@ -1,10 +1,10 @@
 #include "Labyrinthe.h"
 #include "Chasseur.h"
 #include "Gardien.h"
-#include <stdlib.h>
-#include <stdio.h>
+#include <cstring>
 #include <string>
 #include <fstream>
+#include <map>
 #include <iostream>
 
 using namespace std;
@@ -27,10 +27,13 @@ Labyrinthe::Labyrinthe (char* filename){
 	this->_nwall = 0;
 	this->_npicts = 0;
 	this->_nboxes = 0;
-	this->_nguards = 0;
+	this->_nguards = 1;
 	
 	//Le nombres de jonction de mur
 	int nbCorners = 0;
+
+	//Map (lettre, filename) pour les affiches
+	map<char, string> affiches;
 
 	//La ligne courante du fichier
 	string line;
@@ -49,27 +52,32 @@ Labyrinthe::Labyrinthe (char* filename){
 
 			//Définition d'une affiche
 			if (!zoneLaby && line[0] >= 'a' && line[0] <= 'z') {
-				//Traitement affiche
+				affiches[line[0]] = string(line, 2, string::npos);
 			}
 
 			if (zoneLaby) {
 				int sizeLine = line.size();
-				for (int i = 0; i<sizeLine; i++){
+				for (int i = 0; i<sizeLine; i++) {
 					if (line[i] != ' ' && line[i] != 'C') {
 						
-						if (line[i] == '+') {
-							nbCorners++;
+						switch (line[i]) {
+							case '+':
+								nbCorners++;
+								break;
+							case 'X':
+								this->_nboxes++;
+								break;
+							case 'G':
+								this->_nguards++;
+								break;
+							default:
+								if (line[i] >= 'a' && line[i] <= 'z') {
+									this->_npicts++;
+								} else {
+									//TODO: exeption mauvais laby ?
+								}
+								break;
 						}
-						else if (line[i] == 'X') {
-							// TODO: this->_nboxes++;
-						}
-						else if (line[i] == 'G') {
-							// TODO: this->_nguards++;
-						}
-						else if (line[i] >= 'a' && line[i] <= 'z') {
-							// TODO: this->_npicts++;
-						}
-						//else TODO: exeption mauvais laby ?
 					}
 				}
 
@@ -90,6 +98,14 @@ Labyrinthe::Labyrinthe (char* filename){
 
 	//Initialisation de _data
 	this->_data = new char*[this->lab_width];
+
+	//Initialisation des boites
+	this->_boxes = new Box[this->_nboxes];
+	int indexBox = 0;
+
+	// //Initialisation des gardiens
+	this->_guards = new Mover*[this->_nguards];
+	int indexGuardian = 1, hunter_x, hunter_y;
 
     int i = 0;
 	file.open(filename);
@@ -113,6 +129,46 @@ Labyrinthe::Labyrinthe (char* filename){
 
 				if (j < sizeLine) {
 					matFile[i][j] = line[j];
+					
+					switch (line[j]) {
+						case 'X':
+							this->_boxes[indexBox]._x = i;
+							this->_boxes[indexBox]._y = j;
+							this->_boxes[indexBox]._ntex = 0;
+							indexBox++;
+							break;
+						case 'T':
+							this->_treasor._x = i;
+							this->_treasor._y = j;
+							break;
+						case 'G':
+							Mover* guard;
+							switch (indexGuardian%4) {
+								case 1:
+									guard = new Gardien(this, "drfreak");
+									break;
+								case 2:
+									guard = new Gardien(this, "Marvin");
+									break;
+								case 3:
+									guard = new Gardien(this, "Potator");
+									break;
+								default:
+									guard = new Gardien(this, "garde");
+									break;
+							}
+
+							this->_guards[indexGuardian] = guard;
+							this->_guards[indexGuardian]->_x = i*scale;
+							this->_guards[indexGuardian]->_y = j*scale;
+							indexGuardian++;
+							break;
+						case 'C':
+							hunter_x = i;
+							hunter_y = j;
+							break;
+					}
+
 				} else {
 					matFile[i][j] = ' ';
 				}
@@ -128,20 +184,16 @@ Labyrinthe::Labyrinthe (char* filename){
 	}
 
 
-
-    //test affichage fichier
-    for (int a = 0; a < this->lab_width+1; a++) {
-        for (int b = 0; b < this->lab_height+1; b++) {
-            cout<<matFile[a][b];
-        }
-        cout<<endl;
-    }
-
 	//Chaque jonction peut être touché par 4 mur maximum (on prend au plus large)
 	Wall* tooMuchWalls = new Wall[nbCorners*4];
 
 	//On initialise les murs
 	int indexWall = 0;
+
+	//Initialistaion des affiches
+	this->_picts = new Wall[this->_npicts];
+	int indexPicts = 0;
+
 	// Passe mur horizontaux
 	for (int x=0; x<this->lab_width; x++) {
 		int y=0;
@@ -151,6 +203,25 @@ Labyrinthe::Labyrinthe (char* filename){
 				tooMuchWalls[indexWall]._y1 = y;
 				y++;
 				while (matFile[x][y] == '-' || (matFile[x][y] >= 'a' && matFile[x][y] <= 'z')) {
+					//Affichage de l'affiche
+					if (matFile[x][y] >= 'a' && matFile[x][y] <= 'z') {
+						this->_picts[indexPicts]._x1 = x;
+						this->_picts[indexPicts]._y1 = y;
+						this->_picts[indexPicts]._x2 = x;
+						this->_picts[indexPicts]._y2 = y+2;
+						//Si l'affiche est collée à un mur Est (va déborder), on la décale de 1 sur la gauche
+						if (matFile[x][y+1] == '+') {
+							this->_picts[indexPicts]._y1--;
+							this->_picts[indexPicts]._y2--;
+						}
+
+						char* path = new char[128];
+						strcpy(path, this->texture_dir);
+						strcat(path, "/");
+						strcat(path, (affiches[matFile[x][y]]).c_str());
+						this->_picts[indexPicts]._ntex = this->wall_texture(path);
+						indexPicts++;
+					}
 					y++;
 				}
 				//On sort du while donc matFile[x][y] == '+'
@@ -173,6 +244,25 @@ Labyrinthe::Labyrinthe (char* filename){
 				tooMuchWalls[indexWall]._y1 = y;
 				x++;
 				while (matFile[x][y] == '|' || (matFile[x][y] >= 'a' && matFile[x][y] <= 'z')) {
+					//Affichage de l'affiche
+					if (matFile[x][y] >= 'a' && matFile[x][y] <= 'z') {
+						this->_picts[indexPicts]._x1 = x;
+						this->_picts[indexPicts]._y1 = y;
+						this->_picts[indexPicts]._x2 = x+2;
+						this->_picts[indexPicts]._y2 = y;
+						//Si l'affiche est collée à un mur Sud (va déborder), on la décale de 1 vers le haut
+						if (matFile[x+1][y] == '+') {
+							this->_picts[indexPicts]._x1--;
+							this->_picts[indexPicts]._x2--;
+						}
+
+						char* path = new char[128];
+						strcpy(path, this->texture_dir);
+						strcat(path, "/");
+						strcat(path, (affiches[matFile[x][y]]).c_str());
+						this->_picts[indexPicts]._ntex = this->wall_texture(path);
+						indexPicts++;
+					}
 					x++;
 				}
 				//On sort du while donc matFile[x][y] == '+'
@@ -196,34 +286,10 @@ Labyrinthe::Labyrinthe (char* filename){
 		this->_walls[k]._ntex = tooMuchWalls[k]._ntex;
 	}
 
-	delete[] tooMuchWalls;	
+	delete[] tooMuchWalls;
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-	// le chasseur et les 4 gardiens.
-	this->_nguards = 1+4;
-	this->_guards = new Mover* [_nguards];
-	this->_guards [0] = new Chasseur (this);
-	this->_guards [1] = new Gardien (this, "drfreak");
-	this->_guards [2] = new Gardien (this, "Marvin"); _guards [2] -> _x = 90.; _guards [2] -> _y = 70.;
-	this->_guards [3] = new Gardien (this, "Potator"); _guards [3] -> _x = 60.; _guards [3] -> _y = 10.;
-	this->_guards [4] = new Gardien (this, "garde"); _guards [4] -> _x = 130.; _guards [4] -> _y = 100.;
-
-	// indiquer qu'on ne marche pas sur les gardiens.
-	_data [(int)(_guards [1] -> _x / scale)][(int)(_guards [1] -> _y / scale)] = 1;
-	_data [(int)(_guards [2] -> _x / scale)][(int)(_guards [2] -> _y / scale)] = 1;
-	_data [(int)(_guards [3] -> _x / scale)][(int)(_guards [3] -> _y / scale)] = 1;
-	// _data [(int)(_guards [4] -> _x / scale)][(int)(_guards [4] -> _y / scale)] = 1;
+	this->_guards[0] = new Chasseur(this);
+	this->_guards[0]->_x = hunter_x*scale;
+	this->_guards[0]->_y = hunter_y*scale;
 }
